79412ef732adbf42074eafd79c911d8d3a6622cf
44x1carbon <b4079@oic.jp>
2019-03-04 22:17:37 +0900

    add test

---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Test {
+    @Test fun HelloTest() {
+        assertEquals(getText(), "Hello Kotlin")
+    }
+}
